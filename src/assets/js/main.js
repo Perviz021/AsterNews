8d86925d721126b5ba5064a99c4a060dc052d0b1
Parviz Namazli <perviznamazli021@gmail.com>
2023-12-12 14:55:03 +0400

    feat: New UI methods are inserted into main js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,12 @@
 import "../css/index.css";
 
 import Alpine from "alpinejs";
+import { uiNavigator, uiNews, uiSubscription } from "./ui";
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+uiNavigator();
+uiSubscription();
+uiNews();

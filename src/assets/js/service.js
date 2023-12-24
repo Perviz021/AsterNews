9b83d33a728ec20c0ba22aa8db41ab58f5287c86
Parviz Namazli <perviznamazli021@gmail.com>
2023-12-24 12:07:00 +0400

    feat: new methods added for view html

--- src/assets/js/service.js
+++ src/assets/js/service.js
@@ -1,11 +1,22 @@
 import { get } from "./request";
+import { routerLoading } from "./routers";
 
-export const serviceNewsList = async () => {
-  const res = await get("/news");
+export const serviceNewsList = async (params) => {
+  routerLoading(true);
+  const res = await get("/news" + (params ? "?" + params : ""));
+  routerLoading(false);
   return res.data;
 };
 
 export const serviceCategoryList = async () => {
   const res = await get("/news/category");
+  return res;
+};
+
+export const serviceNewsBySlug = async (slug) => {
+  routerLoading(true);
+  const res = await get(`/news/slug/${slug}`);
+  routerLoading(false);
+
   return res;
 };

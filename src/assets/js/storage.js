8c50855aab53a4873e7b750615d854ccdb219203
Parviz Namazli <perviznamazli021@gmail.com>
2023-12-24 12:05:50 +0400

    feat: storage js file created

---
+++ src/assets/js/storage.js
@@ -0,0 +1,7 @@
+export const saveStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getStorage = (key) => {
+  return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : [];
+};

9ce8f762d3a6e0193c05b9b93c88b964c8c2c66e
Parviz Namazli <perviznamazli021@gmail.com>
2023-12-24 12:07:17 +0400

    fix: request method changed

--- src/assets/js/request.js
+++ src/assets/js/request.js
@@ -1,7 +1,9 @@
 const baseURL = "https://all-api.bitcode.az/api";
 
 export const request = async (url, method, params = false) => {
-  let headers = {};
+  let headers = {
+    Accept: "application/json", //geriye normal json qaytarmaq ucun
+  };
 
   let options = {
     method,
@@ -12,7 +14,13 @@ export const request = async (url, method, params = false) => {
 
   const api = await fetch(baseURL + url, options);
 
-  return await api.json();
+  if (api.ok) {
+    return await api.json();
+  } else if (api.status === 404) {
+    return { status: 404 };
+  } else {
+    return { status: 500 };
+  }
 };
 
 export const get = (url) => request(url, "GET");

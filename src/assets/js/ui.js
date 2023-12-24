3c36b74f66686e9e4d51da48157bc1d657dda64e
Parviz Namazli <perviznamazli021@gmail.com>
2023-12-24 12:06:24 +0400

    feat: new methods added for view page

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -1,4 +1,13 @@
-import { serviceCategoryList, serviceNewsList } from "./service";
+import { objectToQueryString } from "./helper";
+import { viewRouter } from "./routers";
+import {
+  serviceCategoryList,
+  serviceNewsBySlug,
+  serviceNewsList,
+} from "./service";
+import { getStorage, saveStorage } from "./storage";
+
+let categories = getStorage("categories");
 
 const getUiTemplate = (id, selector) => {
   const el = document.getElementById(id);
@@ -25,12 +34,14 @@ const getCategoryIcon = (key) => {
 
 export const uiNavigator = async () => {
   const res = await serviceCategoryList();
+  saveStorage("categories", res);
   const navContainer = document.querySelector("#navigation");
   const template = getUiTemplate("navigation-item", ".nav-link");
 
   let html = "";
   res.forEach((i) => {
     let newTemplate = template;
+    newTemplate.href = `/#/search?category=${i.slug}`;
     newTemplate.querySelector("i").classList = `icon ${getCategoryIcon(
       i.slug
     )}`;
@@ -50,8 +61,8 @@ export const uiSubscription = () => {
   content.innerHTML = item.outerHTML;
 };
 
-export const uiNews = async () => {
-  const res = await serviceNewsList();
+export const uiNews = async (params = {}) => {
+  const res = await serviceNewsList(objectToQueryString(params));
   const content = document.querySelector("#news-content");
   const template = getUiTemplate("news-template", "article");
 
@@ -61,10 +72,53 @@ export const uiNews = async () => {
     newTemplate.querySelector("figure img").src = i.photo;
     newTemplate.querySelector(".title").textContent = i.title;
     newTemplate.querySelector(".agency").textContent = i.author.agency;
-    newTemplate.querySelector(".read-later").href = `/view.html?slug=${i.slug}`;
+    newTemplate.querySelector(".read-later").href = `/#/view?slug=${i.slug}`;
 
     html += newTemplate.outerHTML;
   });
 
   content.innerHTML = html;
+};
+
+export const uiNewsView = async (slug) => {
+  const res = await serviceNewsBySlug(slug);
+
+  if (res?.status === 404) {
+    viewRouter("error");
+    return false;
+  }
+
+  const content = document.querySelector("#news-view");
+  const template = getUiTemplate("news-view-content", "div");
+  template.querySelector("#title").textContent = res.title;
+  template.querySelector("#category").textContent = res.category.name;
+  template.querySelector(
+    "#category"
+  ).href = `/#/search?category=${res.category.slug}`;
+  template.querySelector("#photo").src = res.photo;
+  template.querySelector("#content").innerHTML = res.content;
+  content.innerHTML = template.outerHTML;
+  console.log(res);
+};
+
+export const uiNewsSearch = (params) => {
+  const { category } = params;
+  const findCategory = categories.find((i) => i.slug === category);
+  const pageTitle = document.querySelector("#pageTitle");
+
+  pageTitle.textContent = findCategory?.name;
+
+  uiNews(params);
+};
+
+export const UI = {
+  home() {
+    uiNews();
+  },
+  view({ slug }) {
+    uiNewsView(slug);
+  },
+  search(params) {
+    uiNewsSearch(params);
+  },
 };
